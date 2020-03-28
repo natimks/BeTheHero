94709ce3ae89a37f7970168e1676ed2f2d2063b2
natimks <natalia.inf123@gmail.com>
2020-03-28 11:15:26 -0300

    adicao de validacoes e testes do sistema

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,7 +1,8 @@
 const knex = require("knex");
 const configuration = require("../../knexfile");
 
+const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
 // definindo para realizar com conexão com o banco de dados definido para development
-const connection = knex(configuration.development);
+const connection = knex(config);
 
 module.exports = connection;
